2c306d895580950fe3b02e6059e0ca7d2d2374d3
parvin <solent92@gmail.com>
2019-02-13 06:37:51 +0800

    container

---
+++ src/DI/Container.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Eagle\DI;
+
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class Container implements ContainerInterface {
+
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function has($id)
+    {
+        // TODO: Implement has() method.
+    }
+
+}
